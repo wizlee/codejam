64b49ecf18400b859955f4042a9cf19896301779
Wiz Lee <wizlee.dev@gmail.com>
2023-03-26 02:14:42 +0800

    Figured out input and output format codejam is expecting

--- 2008/qualification-round.py
+++ 2008/qualification-round.py
@@ -1,20 +1,9 @@
 '''
-Attempting year 2008 code jam qualification round question
+Codejam 2008; qualification round: Fly Swatter
 - https://codingcompetitions.withgoogle.com/codejam/round/0000000000432b79/0000000000432f32#problem
 '''
 
-
-import argparse
-from pathlib import Path
 from dataclasses import dataclass
-
-sample_input = '''5
-0.250000 1.000000 0.100000 0.010000 0.500000
-0.250000 1.000000 0.100000 0.010000 0.900000
-0.000010 10000.000000 0.000010 0.000010 1000.000000
-0.400000 10000.000000 0.000010 0.000010 700.000000
-1.000000 100.000000 1.000000 1.000000 10.000000
-'''
 
 @dataclass
 class TestCase:
@@ -31,17 +20,11 @@ class Input:
 
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-f", "--inputfile", type=int, help="Filepath to input file")
-
-  args = parser.parse_args()
-  input = args.inputfile
-  input_txt = Path(input).read_text()
-  prods_list = get_probs(input_txt)
+  print_output(get_probs())
 
 
-def get_probs(input_txt):
-  input : Input = parse_input_txt(input_txt)
+def get_probs():
+  input : Input = get_all_input()
   prods_str = []
   for i in range(input.total_input):
     test_case : TestCase = input.test_cases[i]
@@ -50,28 +33,12 @@ def get_probs(input_txt):
   return prods_str
 
 
-# final answer is area of gaps divided by area of racquet
-def get_prod(test_case: TestCase):
-  # research so far
-  # area of a segment of circle: https://www.wolframalpha.com/input?i=segment+of+a+circle
-  # where 0.5 * r^2 * (θ - sinθ)
-  # where θ is in radian
-  # where 2 π radians == 360 degree
-  pass
-
-
-def parse_input_txt(input_txt):
-  input_lines = str.splitlines(input_txt)
-
-  N = 0
+def get_all_input():
+  N = int(input())
   test_cases = []
 
-  for i, line in enumerate(input_lines):
-    if i == 0:
-      N = int(line)
-      continue
-
-    variables = str.split(line, ' ')
+  for _ in range(N):
+    variables = input().split(' ')
     f = float(variables[0])
     R = float(variables[1])
     t = float(variables[2])
@@ -83,5 +50,28 @@ def parse_input_txt(input_txt):
   return Input(N, test_cases)
 
 
+# final answer is area of gaps divided by area of racquet
+def get_prod(test_case: TestCase):
+  # research so far
+  # area of a segment of circle: https://www.wolframalpha.com/input?i=segment+of+a+circle
+  # where 0.5 * r^2 * (θ - sinθ)
+  # where θ is in radian
+  # where 2 π radians == 360 degree
+  return test_case.f + test_case.R + test_case.t + test_case.r + test_case.g
+
+
+def print_output(prods_list):
+  for i, prod in enumerate(prods_list):
+    print(f'Case #{i+1}: {str(prod)}')
+
+
 if __name__ == "__main__":
-    get_probs(sample_input)
+  sample_input = '''5
+0.250000 1.000000 0.100000 0.010000 0.500000
+0.250000 1.000000 0.100000 0.010000 0.900000
+0.000010 10000.000000 0.000010 0.000010 1000.000000
+0.400000 10000.000000 0.000010 0.000010 700.000000
+1.000000 100.000000 1.000000 1.000000 10.000000
+'''
+  # get_probs(sample_input)
+  main()
